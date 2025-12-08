a2292156268a990fec397c21d29dc7e33750f368
Hamid <hamidbawasacca@gmail.com>
2025-12-08 08:24:34 +0100

    forcer en https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,8 +19,14 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        Schema::defaultStringLength(191);
+    
+
+public function boot()
+{
+    Schema::defaultStringLength(191);
+    if ($this->app->environment('production')) {
+        URL::forceScheme('https');
     }
+}
+
 }
